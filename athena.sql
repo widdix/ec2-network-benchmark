e14263b5b65e32ca7533dc092539354c676e8194
Andreas Wittig <andreas@widdix.de>
2018-09-25 07:37:32 +0200

    updated query

--- athena.sql
+++ athena.sql
@@ -30,12 +30,15 @@ SELECT
   (max(interval.sum.bits_per_second)/1000000000) AS max,
   (avg(interval.sum.bits_per_second)/1000000000) AS avg,
   (stddev(interval.sum.bits_per_second)/1000000000) AS stddev,
+  (approx_percentile(interval.sum.bits_per_second, 0.95)/1000000000) AS p95,
   (approx_percentile(interval.sum.bits_per_second, 0.90)/1000000000) AS p90,
   (approx_percentile(interval.sum.bits_per_second, 0.70)/1000000000) AS p70,
-    (approx_percentile(interval.sum.bits_per_second, 0.50)/1000000000) AS p50,
+  (approx_percentile(interval.sum.bits_per_second, 0.50)/1000000000) AS p50,
+  (approx_percentile(interval.sum.bits_per_second, 0.30)/1000000000) AS p30,
+  (approx_percentile(interval.sum.bits_per_second, 0.05)/1000000000) AS p05,
   region, 
   instancetype 
 FROM networkbenchmark CROSS JOIN UNNEST(intervals) WITH ORDINALITY AS t(interval, counter)
-WHERE d >= from_iso8601_date('2018-04-01') AND cardinality(intervals) = 30
+WHERE d >= from_iso8601_date('2018-04-12') AND cardinality(intervals) = 60
 GROUP BY region, instancetype 
 ORDER BY region, instancetype;
